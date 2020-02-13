948839c0114f2c2001f96ca36e66faa22b372618
Jacob Rose <rose030@gannon.edu>
2020-02-13 09:47:00 -0500

    completed class structure for salaryCalculator

---
+++ src/assignment1salarycalculator/salaryCalculator.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1salarycalculator;
+
+/**
+ *
+ * @author jaker
+ */
+public class salaryCalculator {
+    
+    public double calcSalary(double hourlyRate){
+        return 0; //dont't forget to change later!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    }
+    
+    public double calcHourly(double salary){
+        return 0; //dont't forget to change later!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    }
+    
+    public double takeHomePerMonth(double salary, double taxRate){
+        return 0; //dont't forget to change later!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    }
+    
+    public double calcTaxes(double salary, double taxRate){
+      return 0; //dont't forget to change later!!!!!!!!!!!!!!!!!!!!!!!!!!!!  
+    }
+}
